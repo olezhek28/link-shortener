e195a698531a519b6827a903ea0dd0cd2f4ba580
Sergey <serg3091@gmail.com>
2022-05-03 13:46:49 +0300

    fix: :bug: fix user and password

--- migration.local.sh
+++ migration.local.sh
@@ -3,9 +3,9 @@
 export MIGRATION_DIR=./migrations
 export DB_HOST="localhost"
 export DB_PORT="5444"
-export DB_NAME="sample_db"
-export DB_USER="postgres"
-export DB_PASSWORD="sample_pass"
+export DB_NAME="likns"
+export DB_USER="links"
+export DB_PASSWORD="jw8s0F4"
 export DB_SSL=disable
 
 export PG_DSN="host=${DB_HOST} port=${DB_PORT} dbname=${DB_NAME} user=${DB_USER} password=${DB_PASSWORD} sslmode=${DB_SSL}"
