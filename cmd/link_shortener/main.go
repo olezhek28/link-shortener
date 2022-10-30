59b299414ea4c901cdffd4a7c45dc6e1863b8ce5
Oleg Kozyrev <okozyrev@ozon.ru>
2022-10-30 16:24:35 +0600

    clean up

--- cmd/link_shortener/main.go
+++ cmd/link_shortener/main.go
@@ -4,35 +4,14 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"time"
 
 	"github.com/olezhek28/link-shortener/internal/app"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-)
-
-func recordMetrics() {
-	go func() {
-		for {
-			opsProcessed.Inc()
-			time.Sleep(2 * time.Second)
-		}
-	}()
-}
-
-var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "processed_ops_total",
-		Help: "The total number of processed events",
-	})
 )
 
 func main() {
 	ctx := context.Background()
-
-	recordMetrics()
-
+	
 	go func() {
 		http.Handle("/metrics", promhttp.Handler())
 		// nolint
